dbfa5a1ad43dded501db3430b6b892a3721157d9
mohandholu43 <mohandholu43@gmail.com>
2024-03-19 10:57:32 +0530

    fix

--- src/utils/passwordRegex.js
+++ src/utils/passwordRegex.js
@@ -2,4 +2,4 @@ export const passwordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/;
 
 export const passwordText =
-  "Password must be at least 8 characters and include a mix of uppercase, lowercase, a number, and a special character.";
+  "Password must be at least 8 characters and include a mix of uppercase, lowercase, a number, and a special character (e.g., !@#$%^&*).";
