eceb7d9cef741eac58e60f0d386933f49aa7298e
rushil95 <rushild95@gmail.com>
2024-03-19 11:43:01 +0530

    Updated server error page title

--- src/server/axios.js
+++ src/server/axios.js
@@ -4,20 +4,17 @@ const Axios = require("axios");
 const { baseURL } = require("../axios");
 const { getProviderToken } = require("./providerToken");
 
-//The server axios is different from the client axios instance
-//This will use service provider's JWT token for auth
+//The server uses a separate axios instance 
+//This instance will use service provider's JWT token for auth
 const axios = Axios.create({
   baseURL,
 });
 
 axios.interceptors.request.use((config) => {
-  // if (config.headers.Authorization === undefined) {
   const providerAuthToken = getProviderToken();
   if (providerAuthToken) {
     config.headers.Authorization = `Bearer ${providerAuthToken}`;
-    // axios.defaults.headers["Authorization"] = `Bearer ${providerAuthToken}`;
   }
-  // }
   return config;
 });
 
