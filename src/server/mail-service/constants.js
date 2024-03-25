b9187f3d1f86a4033cfabe6bc7bccd47ba2f92b3
rushil95 <46681749+rushil95@users.noreply.github.com>
2024-03-25 19:16:10 +0530

    Billing page (#88)

--- src/server/mail-service/constants.js
+++ src/server/mail-service/constants.js
@@ -3,6 +3,7 @@ const eventTypes = {
   InviteUser: "InviteUser",
   RevokeUserRole: "RevokeUserRole",
   ResetPassword: "ResetPassword",
+  InvoiceCreated: "InvoiceCreated",
 };
 
 module.exports = { eventTypes };
