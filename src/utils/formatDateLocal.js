c9bb0f979f026c012f5e040592d283a4ba38b6fe
Dudi Zimberknopf <dudi@vizo.dev>
2024-04-14 10:15:38 +0300

    add fix for unknown timezone

--- src/utils/formatDateLocal.js
+++ src/utils/formatDateLocal.js
@@ -1,5 +1,8 @@
 function formatDateLocal(timestamp) {
   const zone = new Date().toLocaleTimeString(undefined, { timeZoneName: 'short' }).split(' ')[2]
+  if (!zone) {
+    return new Date(timestamp).toLocaleString();
+  }
   return new Date(timestamp).toLocaleString() + " " + zone;
 }
 
