c779ed0e5e9021a7930125fddbca344d4476e71f
nihalmohammed18 <nihalmohammed1804@gmail.com>
2024-05-20 16:04:01 +0800

    Fix: Syntax Error

--- src/hooks/useResourceInstance.js
+++ src/hooks/useResourceInstance.js
@@ -116,7 +116,7 @@ export default function useResourceInstance(
             });
           }
         }
-        if (topologyDetails?.hasCompute) {
+
         if (topologyDetails?.hasCompute) {
           if (topologyDetails?.nodes) {
             topologyDetails.nodes.forEach((node) => {
