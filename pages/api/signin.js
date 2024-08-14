04b24118591890cde137534362c307e6e14e9f2b
rushil95 <46681749+rushil95@users.noreply.github.com>
2024-08-14 19:01:12 +0530

    Add random delay to signin api response (#275)

--- pages/api/signin.js
+++ pages/api/signin.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 const { customerUserSignIn } = require("src/server/api/customer-user");
 const { getEnvironmentType } = require("src/server/utils/getEnvironmentType");
 
@@ -7,7 +9,7 @@ export default async function handleSignIn(nextRequest, nextResponse) {
 
   // console.log("Client IP", clientIP);
   // console.log("Pod IP", process.env.POD_IP);
-  
+
   if (nextRequest.method === "POST") {
     try {
       const environmentType = getEnvironmentType();
@@ -17,10 +19,26 @@ export default async function handleSignIn(nextRequest, nextResponse) {
       };
 
       const response = await customerUserSignIn(payload);
+      const delayInMilliseconds = _.random(0, 60);
+
+      //Wait for a random duration b/w 0ms and 60ms to mask the difference b/w response times of api when a user is present vs not present
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, delayInMilliseconds);
+      });
       nextResponse.status(200).send({ ...response.data });
     } catch (error) {
       let defaultErrorMessage =
         "Failed to sign in. Either the credentials are incorrect or the user does not exist";
+
+      //Wait for a random duration b/w 0ms and 60ms to mask the difference b/w response times of api when a user is present vs not present
+      const delayInMilliseconds = _.random(0, 60);
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, delayInMilliseconds);
+      });
 
       if (
         error.name === "ProviderAuthError" ||
