a8c5650de18afb444a84e2c04fec54493ed74a1e
rushil95 <rushild95@gmail.com>
2024-11-14 22:28:15 +0530

    Update search and refresh icons

--- src/components/Icons/SearchLens/SearchLens.jsx
+++ src/components/Icons/SearchLens/SearchLens.jsx
@@ -1,17 +1,16 @@
-import * as React from "react";
 const SearchLens = (props) => (
   <svg
-    width={20}
-    height={20}
+    width="20"
+    height="20"
     viewBox="0 0 20 20"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     <path
-      d="M17.5 17.5L14.5834 14.5833M16.6667 9.58333C16.6667 13.4954 13.4954 16.6667 9.58333 16.6667C5.67132 16.6667 2.5 13.4954 2.5 9.58333C2.5 5.67132 5.67132 2.5 9.58333 2.5C13.4954 2.5 16.6667 5.67132 16.6667 9.58333Z"
+      d="M19 19L14.65 14.65M17 9C17 13.4183 13.4183 17 9 17C4.58172 17 1 13.4183 1 9C1 4.58172 4.58172 1 9 1C13.4183 1 17 4.58172 17 9Z"
       stroke="#667085"
-      strokeWidth={1.66667}
+      strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
