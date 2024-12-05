1ee93874b9f5dc52b1c4cbb0e863f338fd0bbc19
nihalmohammed18 <129756850+nihalmohammed18@users.noreply.github.com>
2024-12-05 22:04:09 +0530

    Fix: Node Status Tooltip (#445)

--- src/components/ResourceInstance/NodesTable/NodeStatus.jsx
+++ src/components/ResourceInstance/NodesTable/NodeStatus.jsx
@@ -1,11 +1,11 @@
 import { Box } from "@mui/material";
-import { useEffect, useState } from "react";
 import NodeStatusTooltip from "./NodeStatusTooltip";
-import { getChipStyles } from "src/components/StatusChip/StatusChip";
+import StatusChip, {
+  getChipStyles,
+} from "src/components/StatusChip/StatusChip";
 
 export const NodeStatus = (props) => {
   const { detailedHealth, isStopped } = props;
-  const [currentDot, setCurrentDot] = useState(5); // 5 means all dots lit
   const ConnectivityStatus = detailedHealth?.ConnectivityStatus;
   const DiskHealth = detailedHealth?.DiskHealth;
   const NodeHealth = detailedHealth?.NodeHealth;
@@ -19,39 +19,39 @@ export const NodeStatus = (props) => {
     ProcessLiveness,
   ];
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      const interval = setInterval(() => {
-        setCurrentDot((prev) => (prev <= 0 ? 5 : prev - 1));
-      }, 200);
-      return () => clearInterval(interval);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <NodeStatusTooltip detailedHealth={detailedHealth}>
       <Box
-        bgcolor="#ECFDF3"
         display="flex"
         alignItems="center"
         gap="6px"
         borderRadius="16px"
-        p="4px 8px"
+        p="5px 8px"
       >
         {detailedHealthStatus.map((status, index) => {
           const chipStyles = getChipStyles(isStopped ? "UNKNOWN" : status);
+
+          if (isStopped) {
+            return <StatusChip key={index} status="N/A" />;
+          }
+
           return (
             <Box
               key={index}
-              bgcolor={chipStyles.color || "#363F72"}
-              width="7px"
-              height="7px"
+              bgcolor={chipStyles.color || "gray"}
+              width="8px"
+              height="8px"
               borderRadius="50%"
               sx={{
-                opacity: index >= currentDot ? 0.25 : 1,
-                transition: `opacity 0.1s linear`,
+                animation: "blink 1s infinite",
+                "@keyframes blink": {
+                  "0%, 100%": {
+                    opacity: 0.85,
+                  },
+                  "50%": {
+                    opacity: 0.25,
+                  },
+                },
               }}
             />
           );
