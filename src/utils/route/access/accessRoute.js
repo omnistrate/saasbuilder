31d894a20871c3869d3253d0dc5aef9dc7ecb966
nihalmohammed18 <129756850+nihalmohammed18@users.noreply.github.com>
2024-12-13 17:58:20 +0530

    Fix: Redirect Page (#465)
    * Fix: Redirect Page
    
    * Fix

--- src/utils/route/access/accessRoute.js
+++ src/utils/route/access/accessRoute.js
@@ -19,6 +19,7 @@ export function getMarketplaceProductTierRoute(
   if (serviceId) {
     return `/service-plans?serviceId=${serviceId}`;
   }
+  return "/service-plans";
 }
 
 export function getDashboardRoute(
